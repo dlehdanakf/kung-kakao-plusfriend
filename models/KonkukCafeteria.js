ca0fbe57251d6232e82d50ebe22277a05e51258b
KUNG <dlehdanakf@konkuk.ac.kr>
2018-02-19 16:16:23 +0900

    modify cafeteria menu format

--- models/KonkukCafeteria.js
+++ models/KonkukCafeteria.js
@@ -16,15 +16,13 @@ function KonkukCafeteria(d){
                     while(sub_menu.indexOf('.') != -1){ sub_menu = sub_menu.replace('.', ' '); }
                     while(sub_menu.indexOf('  ') != -1){ sub_menu = sub_menu.replace('  ', ' '); }
                     sub_menu = sub_menu.trim();
-                    var sub_menu_arr = sub_menu.split(',');
+                    var sub_menu_arr = sub_menu.split(' ');
                     sub_menu = sub_menu.replace(' ', ', ');
-
-                    console.log(sub_menu);
 
                     var main_menu = m.name.trim();
                     if(!main_menu || main_menu == '' || main_menu == 'Cafeteria'){
                         main_menu = sub_menu_arr[0];
-                        sub_menu_arr = sub_menu_arr.splice(0, 1);
+                        sub_menu_arr.splice(0, 1);
                         sub_menu = sub_menu_arr.join(', ');
                     }
 
@@ -71,14 +69,14 @@ function KonkukCafeteria(d){
         NTIyNzA0: '학생회관(아워홈)',
         NTMyODA5: '학생회관(신세계)',
         NTQyOTE2: '상허도서관(아워홈)',
-        NTQyOTE2: '새천년관(교직원식당)',
+        NTEyNjAx: '새천년관(교직원식당)',
         MjEzMzYyNzIx: '쿨하우스(기숙사)'
     };
 
     var fetchMenu = async(function(){
         return await(
             fetch(
-                'https://bablabs.com/openapi/v1/campuses/MoCQZdj1hE/stores?date=2018-02-06', {
+				'https://bablabs.com/openapi/v1/campuses/MoCQZdj1hE/stores?date=' + getToday(), {
                     method: 'GET',
                     headers: {
                         accesstoken: 'q5U2EaUewn0MbtUaIOjg1LQA5OyGyqe7BJeMuaBn1O4cJQnW4l',
@@ -92,22 +90,29 @@ function KonkukCafeteria(d){
     this.fetch_status = 500;
     this.fetch_result = [];
     this.getStoreMenu = function(){
-        var return_string = getTodayFormat() + ' 학식정보 \n';
+        var return_string = getTodayFormat() + ' 학식정보';
 
         for(var v in restaurant){
-            return_string += '\n<' + restaurant[v] + '>\n';
+	        console.log(restaurant[v]);
+            return_string += '\n\n<' + restaurant[v] + '>';
 
             for(var m in this.fetch_result[v]){
-                console.log(this.fetch_result[v][m]);
-                return_string += '=> ' + this.fetch_result[v][m].main + '\n';
-                return_string += '   ' + this.fetch_result[v][m].sub + '\n';
+                return_string += '\n=> ' + this.fetch_result[v][m].main;
+                if(this.fetch_result[v][m].arr.length > 0)
+                    return_string += '\n     ' + this.fetch_result[v][m].sub;
             }
+            if(this.fetch_result[v].length < 1)
+                return_string += '\n=> 식당에서 식단을 업로드하지 않았습니다.';
+            
+            return_string += '\n\n====================';
         }
+        
+        return_string += '\n상기 내용은 컴퓨터에 의해 자동으로 가져온 정보로 식당 운영사정 및 기타 사유로 인해 정확하지 않을 수 있습니다.'
 
         return return_string;
     }
 
-    async(constructor(d ? d : getToday()));
+    async(constructor());
 };
 
 module.exports = KonkukCafeteria;
